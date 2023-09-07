a0833721faa77e2a2eee8de6c98910633c301542
Inam Ul Huq <inambe.dev@gmail.com>
2023-09-08 02:18:07 +0500

    add to-top button

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ feather.replace();
 
 const mainMenuBtn = document.querySelector("#main-menu-btn");
 const mainMenu = document.querySelector("#main-menu");
+const toTopBtn = document.querySelector("#to-top");
 
 mainMenuBtn.addEventListener("click", () => {
   mainMenu.classList.toggle("main-menu__open");
+});
+
+toTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
