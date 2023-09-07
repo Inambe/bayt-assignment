63159762231b6d768fb03f7bb6760f7cf29db4e6
Inam Ul Huq <inambe.dev@gmail.com>
2023-09-07 23:02:30 +0500

    add mobile menu

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('hello')
+// init the icons
+feather.replace();
+
+const mainMenuBtn = document.querySelector("#main-menu-btn");
+const mainMenu = document.querySelector("#main-menu");
+
+mainMenuBtn.addEventListener("click", () => {
+  mainMenu.classList.toggle("main-menu__open");
+});
